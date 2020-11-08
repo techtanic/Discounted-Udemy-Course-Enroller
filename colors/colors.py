03c5c199c72c4d597adc49ced80d182d9aded9fa
TECHTANIC <64263079+techtanic@users.noreply.github.com>
2020-11-08 12:15:30 +0300

    improvements

---
+++ colors/colors.py
@@ -0,0 +1,26 @@
+from colorama import init, Fore, Back, Style
+
+init(autoreset=True)
+# colors foreground text:
+fc = Fore.CYAN
+fg = Fore.GREEN
+fw = Fore.WHITE
+fr = Fore.RED
+fb = Fore.BLUE
+fy = Fore.YELLOW
+fm = Fore.MAGENTA
+
+
+# colors background text:
+bc = Back.CYAN
+bg = Back.GREEN
+bw = Back.WHITE
+br = Back.RED
+bb = Back.BLUE
+by = Fore.YELLOW
+bm = Fore.MAGENTA
+
+# colors style text:
+sd = Style.DIM
+sn = Style.NORMAL
+sb = Style.BRIGHT
