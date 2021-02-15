1a49a943261df92f3efaca9d63917dc020d99516
Anish Gowda <anishgowda43@gmail.com>
2021-02-15 23:28:38 +0530

    changed funtions.py

--- pack/functions.py
+++ pack/functions.py
@@ -14,8 +14,7 @@ def udemy_freebies(page):
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
     }
 
-    r = requests.get("https://www.udemyfreebies.com/free-udemy-courses/" +
-                     str(page), headers=head, verify=False)
+    r = requests.get(UDEMYFREEBIES + str(page), headers=head, verify=False)
     soup = BeautifulSoup(r.content, 'html.parser')
     all = soup.find_all('div', 'theme-block')
     for index, items in enumerate(all):
