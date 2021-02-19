013f84da5f8666ef180110ec71d7459d60187267
TECHTANIC <64263079+techtanic@users.noreply.github.com>
2021-02-19 12:36:20 +0300

    update

--- pack/functions.py
+++ pack/functions.py
@@ -6,54 +6,55 @@ from .constants import *
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-def udemy_freebies(page):
+def udemy_freebies():
     links_ls = []
     head = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
     }
-    
-    r = requests.get(UDEMYFREEBIES + str(page), headers=head, verify=False)
-    soup = BeautifulSoup(r.content, 'html.parser')
-    all = soup.find_all('div', 'theme-block')
-    for index, items in enumerate(all):
-        title = items.img['title']
-        url2 = items.a['href']
-        r2 = requests.get(url2, headers=head, verify=False)
-        soup1 = BeautifulSoup(r2.content, 'html.parser')
-        url3 = soup1.find('a', class_ = 'button-icon')['href']
-        link = requests.get(url3, verify=False).url
-        links_ls.append(title + '|:|' + link)
-    return links_ls
-
-def discudemy(page):
-    links_ls = []
-    head = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
-    }
-    
-    r = requests.get(DISCUD + str(page), headers=head, verify=False)
-    soup = BeautifulSoup(r.content, 'html.parser')
-    all = soup.find_all('section', 'card')
-    for index, items in enumerate(all):
-        try:
-            title = items.a.text
+    for page in range(1,4):
+        r = requests.get('https://www.udemyfreebies.com/free-udemy-courses/' + str(page), headers=head, verify=False)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        all = soup.find_all('div', 'theme-block')
+        for index, items in enumerate(all):
+            title = items.img['title']
             url2 = items.a['href']
-        except:
-            title = ''
-            url2 = ''
-        if url2 != '':
             r2 = requests.get(url2, headers=head, verify=False)
             soup1 = BeautifulSoup(r2.content, 'html.parser')
-            next = soup1.find('div', 'ui center aligned basic segment')
-            url3 = next.a['href']
-            r3 = requests.get(url3, headers=head, verify=False)
-            soup3 = BeautifulSoup(r3.content, 'html.parser')
-            links_ls.append(title + '|:|' + soup3.find('div', 'ui segment').a['href'])
+            url3 = soup1.find('a', class_ = 'button-icon')['href']
+            link = requests.get(url3, verify=False).url
+            links_ls.append(title + '|:|' + link)
     return links_ls
 
-def course_mania(page):
+def discudemy():
+    links_ls = []
+    head = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+    }
+
+    for page in range(1,4):
+        r = requests.get('https://www.discudemy.com/all/' + str(page), headers=head, verify=False)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        all = soup.find_all('section', 'card')
+        for index, items in enumerate(all):
+            try:
+                title = items.a.text
+                url2 = items.a['href']
+            except:
+                title = ''
+                url2 = ''
+            if url2 != '':
+                r2 = requests.get(url2, headers=head, verify=False)
+                soup1 = BeautifulSoup(r2.content, 'html.parser')
+                next = soup1.find('div', 'ui center aligned basic segment')
+                url3 = next.a['href']
+                r3 = requests.get(url3, headers=head, verify=False)
+                soup3 = BeautifulSoup(r3.content, 'html.parser')
+                links_ls.append(title + '|:|' + soup3.find('div', 'ui segment').a['href'])
+    return links_ls
+
+def course_mania():
     links_ls = []
     head = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
@@ -62,7 +63,7 @@ def course_mania(page):
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
     }
     
-    r = requests.get(COURSEMANIA, headers=head, verify=False)
+    r = requests.get('https://api.coursemania.xyz/api/get_courses', headers=head, verify=False)
     js = r.json()
     for items in js:
         title = items['courseName']
